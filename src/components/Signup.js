9e2305e7adcb306608b4a523e8c536895745c3fd
program_with_abhi <programwithabhi@gmail.com>
2022-09-11 14:09:06 +0530

    empty

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -24,6 +24,9 @@ function Signup(props) {
       })
       const res = await response.json()
       console.log(res)
+      if(res.error){
+        showAlert('User with this email already exist try another email','warning')
+      }
       if (res.success) {
         localStorage.setItem("authtoken", res.authToken)
         history("/")
@@ -35,7 +38,7 @@ function Signup(props) {
       }
     }
     else {
-      console.log("Retype password correctly")
+      showAlert("Password do not match",'danger');
     }
   }
   let onChange = (e) => {
@@ -49,20 +52,20 @@ function Signup(props) {
         <form className='login-form' onSubmit={handleSubmit}>
           <div className="mb-3">
             <label htmlFor="name" className="form-label">Name</label>
-            <input onChange={onChange} value={credentials.name} name='name' type="text" className="form-control" id="name" />
+            <input required onChange={onChange} value={credentials.name} name='name' type="text" className="form-control" id="name" />
           </div>
           <div className="mb-3">
             <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-            <input name='email' onChange={onChange} value={credentials.email} type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+            <input required name='email' onChange={onChange} value={credentials.email} type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
             <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
           </div>
           <div className="mb-3">
             <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-            <input name='password' onChange={onChange} value={credentials.password} type="password" className="form-control" id="exampleInputPassword1" />
+            <input required minLength={6} name='password' onChange={onChange} value={credentials.password} type="password" className="form-control" id="exampleInputPassword1" />
           </div>
           <div className="mb-3">
             <label htmlFor="retype" className="form-label">Retype-Password</label>
-            <input name='retype' onChange={onChange} type="password" className="form-control" id="retype" />
+            <input required minLength={6} name='retype' onChange={onChange} type="password" className="form-control" id="retype" />
           </div>
           <div className='button-div'>
             <button type="submit" className="btn btn-primary login-button">SignUp</button>
